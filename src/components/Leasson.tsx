93fe8d8b3f4ff7f6ff6299232abe705fdb282ae5
unknown <tmrafinha4@gmail.com>
2022-06-23 22:42:34 -0300

    230602

--- src/components/Leasson.tsx
+++ src/components/Leasson.tsx
@@ -1,22 +1,46 @@
-export function Leasson(){
+import { CheckCircle, Lock } from 'phosphor-react'
+import { isPast, format } from 'date-fns'
+
+interface LessonProps {
+    title: string;
+    slug: string;
+    availableAt: Date;
+    type: 'live' | 'class';
+}
+
+export function Leasson(props: LessonProps){
+
+    const isLessonAvaiable = isPast(props.availableAt);
+    const avaiableDateFormatted = format(props.availableAt, "d' 'MMMM' • 'k'h'mm")
+
     return (
         <a href="#">
             <span className="text-gray-300">
-                Segunda • 21 de junho • 19h00
+                {avaiableDateFormatted}
             </span>
 
             <div className="rounded border border-gray-500 p-4 mt-2">
                 <header className="flex items-center justify-between">
-                    <span className="text-sm text-blue-500 font-medium">
+                    
+                    {isLessonAvaiable ? (
+                        <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+                        <CheckCircle size={20}/>
                         Conteúdo liberado
                     </span>
+                    ) : (
+                        <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+                        <Lock size={20}/>
+                        Em breve
+                    </span>
+                    )}
+
                     <span className="text-xs rounded px-2 py[2px] text-white border border-green-300 font-bold">
-                        AO VIVO
+                        {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
                     </span>
                 </header>
 
                 <strong className="text-gray-200 mt-5 block">
-                    Abertura do evento Ignire lab
+                    {props.title}
                 </strong>
             </div>
         </a>

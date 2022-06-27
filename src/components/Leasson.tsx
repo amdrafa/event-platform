8b7d14268ac4d2e83f8d03fcbc4d23a8652bb13d
unknown <tmrafinha4@gmail.com>
2022-06-27 06:48:28 -0300

    almostlast

--- src/components/Leasson.tsx
+++ src/components/Leasson.tsx
@@ -1,5 +1,6 @@
 import { CheckCircle, Lock } from 'phosphor-react'
 import { isPast, format } from 'date-fns'
+import { Link, useParams } from 'react-router-dom';
 
 interface LessonProps {
     title: string;
@@ -13,17 +14,21 @@ export function Leasson(props: LessonProps){
     const isLessonAvaiable = isPast(props.availableAt);
     const avaiableDateFormatted = format(props.availableAt, "d' 'MMMM' • 'k'h'mm")
 
+    const { slug } = useParams<{slug: string;}>()
+
+    const isActiveLesson = slug === props.slug
+
     return (
-        <a href="#">
+        <Link to={`/event/lesson/${props.slug}`} className='group'>
             <span className="text-gray-300">
                 {avaiableDateFormatted}
             </span>
 
-            <div className="rounded border border-gray-500 p-4 mt-2">
+            <div className={`rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 ${isActiveLesson ? 'bg-green-500' : ''}`}>
                 <header className="flex items-center justify-between">
                     
                     {isLessonAvaiable ? (
-                        <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+                        <span className={`text-sm ${!isActiveLesson ? 'text-blue-500' : 'text-white'} font-medium flex items-center gap-2`}>
                         <CheckCircle size={20}/>
                         Conteúdo liberado
                     </span>
@@ -39,10 +44,10 @@ export function Leasson(props: LessonProps){
                     </span>
                 </header>
 
-                <strong className="text-gray-200 mt-5 block">
+                <strong className={`${isActiveLesson? 'text-white' : 'text-gray-200'} mt-5 block`}>
                     {props.title}
                 </strong>
             </div>
-        </a>
+        </Link>
     )
 }
